75946e6f5a0074afca84d32ddfb5eff78091f9ba
gsvic <victorasgs@gmail.com>
2016-10-28 15:02:26 +0300

    first commit

---
+++ socialskip.js
@@ -0,0 +1,12 @@
+document.getElementById('btn').addEventListener("click", makeExperiment)
+
+console.log("Added.")
+
+function makeExperiment(){
+        var videoId = document.getElementById("videoid").value
+        console.log(videoId)
+        document.getElementById("experiment").innerHTML = '\
+        <h2>Press the START button to start the experiment</h2>\
+        <iframe src="http://www.socialskip.org/watch?videoId=' + videoId +'" width="600px" height="450px">\
+        </iframe>'
+ }
